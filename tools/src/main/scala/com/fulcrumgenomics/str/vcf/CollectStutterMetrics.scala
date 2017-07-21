b6c90b28462df6adff167c34c35993de87ac4d72
Nils Homer <nh13@users.noreply.github.com>
2017-07-20 20:08:25 -0700

    Adding a few things to the stutter plots (#23)

--- tools/src/main/scala/com/fulcrumgenomics/str/vcf/CollectStutterMetrics.scala
+++ tools/src/main/scala/com/fulcrumgenomics/str/vcf/CollectStutterMetrics.scala
@@ -28,7 +28,7 @@ package com.fulcrumgenomics.str.vcf
 import com.fulcrumgenomics.FgBioDef.{PathPrefix, PathToIntervals, PathToVcf}
 import com.fulcrumgenomics.cmdline.ClpGroups
 import com.fulcrumgenomics.commons.io.{Io, PathUtil}
-import com.fulcrumgenomics.commons.util.{LazyLogging, NumericCounter}
+import com.fulcrumgenomics.commons.util.{LazyLogging, NumericCounter, SimpleCounter}
 import com.fulcrumgenomics.sopt.{arg, clp}
 import com.fulcrumgenomics.str.cmdline.FgStrTool
 import htsjdk.variant.vcf.VCFFileReader
@@ -36,7 +36,7 @@ import com.fulcrumgenomics.commons.CommonsDef.SafelyClosable
 import com.fulcrumgenomics.commons.CommonsDef._
 import com.fulcrumgenomics.str.vcf.StrInterval.StrAllele
 import com.fulcrumgenomics.util.Rscript
-import htsjdk.variant.variantcontext.VariantContext
+import htsjdk.variant.variantcontext.{Allele, VariantContext}
 
 import scala.collection.mutable
 import scala.util.Failure
@@ -107,8 +107,8 @@ class CollectStutterMetrics
       }
 
       if (refCalls.isEmpty) {
-        counters.add(str.name, new NumericCounter[Int]())
-        hipStrCounters.add(str.name, new NumericCounter[Int]())
+        counters.add(str.name, StutterAndAlleleCounter.empty)
+        hipStrCounters.add(str.name, StutterAndAlleleCounter.empty)
       }
       else {
         // genotyped bam
@@ -122,19 +122,18 @@ class CollectStutterMetrics
     }
 
     // genotyped bam
-    val stutterOut = PathUtil.pathTo(output + ".stutter.tab")
-    counters.write(PathUtil.pathTo(output + ".stutter.tab"), strIntervals, minZeroStutter)
+    counters.write(output, strIntervals, minZeroStutter)
 
     // raw reads
-    val rawStutterOut = PathUtil.pathTo(output + ".stutter.raw.tab")
-    hipStrCounters.write(rawStutterOut, strIntervals, minZeroStutter)
+    hipStrCounters.write(PathUtil.pathTo(output + ".raw"), strIntervals, minZeroStutter)
 
     vcfIn.safelyClose()
 
+    /*
     if (!skipPlots) {
       logger.info("Plotting")
       val pdf = PathUtil.pathTo(output + ".stutter.pdf")
-      Rscript.execIfAvailable(ScriptPath, stutterOut.toString, rawStutterOut.toString, pdf.toString) match {
+      Rscript.execIfAvailable(ScriptPath, output.toString, pdf.toString) match {
         case Failure(e) => logger.warning(s"Generation of PDF plots failed: ${e.getMessage}")
         case _ => Unit
       }
@@ -142,6 +141,7 @@ class CollectStutterMetrics
     else {
       logger.warning("Skipping plots")
     }
+    */
   }
 
   // find the one closes to zero
@@ -152,33 +152,38 @@ class CollectStutterMetrics
 
   /** Gets the calls for this STR and returns a [[NumericCounter]] that counts the # of observations for
     * each stutter event. */
-  private def process(str: StrInterval, vcfIn: VCFFileReader, refCalls: Seq[Float]): NumericCounter[Int] = {
+  private def process(str: StrInterval, vcfIn: VCFFileReader, refCalls: Seq[Float]): StutterAndAlleleCounter = {
     vcfIn.query(str.chrom, str.start, str.end)
       .filter(ctx => ctx.getNoCallCount != ctx.getNSamples)
       .toSeq match {
-        case Seq(ctx) => if (ctx.getNSamples == 1) collectSingle(str, ctx, refCalls) else collectRaw(str, ctx, refCalls)
-        case calls => new NumericCounter[Int]()
+        case Seq(ctx) => if (ctx.getNSamples == 1) collectSingle(str, ctx, refCalls) else {
+          collectRaw(str, ctx, refCalls)
+        }
+        case calls    => StutterAndAlleleCounter.empty
       }
   }
 
   /** Given an STR and set of calls, returns a [[NumericCounter]] that counts the # of observations for
     * each stutter event. */
-  private def collect(str: StrInterval, refCalls: Seq[Float], calls: Seq[StrAllele]): NumericCounter[Int] = {
-    val counter = new NumericCounter[Int]()
+  private def collect(refCalls: Seq[Float], calls: Seq[StrAllele]): StutterAndAlleleCounter = {
+    val stutterCounter = new NumericCounter[Int]()
+    val alleleCounter = new SimpleCounter[String]()
+
     calls.filter(_.count > 0)
       .foreach { call =>
         val stutter = {
           val abs = minAbs(refCalls.map(refCall => call.repeatLength - refCall):_*)
           if (abs < 0) Math.ceil(abs) else Math.floor(abs)
         }.toInt
-        counter.count(stutter, call.count)
+        stutterCounter.count(stutter, call.count)
+        alleleCounter.count(call.allele.getBaseString, call.count)
       }
-    counter
+    StutterAndAlleleCounter(stutterCounter, alleleCounter)
   }
 
   /** Assumes a variant context produced by `StrGenotypeDuplexMolecules` and returns a [[NumericCounter]] that counts
     * the # of observations for each stutter event. */
-  private def collectSingle(str: StrInterval, ctx: VariantContext, refCalls: Seq[Float]): NumericCounter[Int] = {
+  private def collectSingle(str: StrInterval, ctx: VariantContext, refCalls: Seq[Float]): StutterAndAlleleCounter = {
     // Get all calls seen, not just the one genotyped
     val counts = {
       val refCount  = ctx.getAttributeAsInt("REFAC", 0)
@@ -187,13 +192,13 @@ class CollectStutterMetrics
     }
     val calls = StrAllele.toCalls(str, ctx, counts)
 
-    collect(str, refCalls, calls)
+    collect(refCalls, calls)
   }
 
   /** Assumes a variant context produced by `HipSTR` and returns a [[NumericCounter]] that counts
     * the # of observations for each stutter event. */
-  private def collectRaw(str: StrInterval, ctx: VariantContext, refCalls: Seq[Float]): NumericCounter[Int] = {
-    val counter = new NumericCounter[Int]()
+  private def collectRaw(str: StrInterval, ctx: VariantContext, refCalls: Seq[Float]): StutterAndAlleleCounter = {
+    val counter = StutterAndAlleleCounter()
 
     ctx.getGenotypes.filterNot(_.isNoCall).foreach { genotype =>
       require(genotype.getAlleles.length == 1, s"Expected a single allele, found '${genotype.getAlleles.length}'")
@@ -206,13 +211,20 @@ class CollectStutterMetrics
             case Seq(bpDiff, count) =>
               // NB: bpDiff is the # of bases different relative to the reference!
               val alleleLength = bpDiff.toInt + (str.refLength * str.unitLength)
-              StrAllele(allele=null, alleleLength=alleleLength, count=count.toInt, unitLength=str.unitLength)
+              val alleleString = if (alleleLength > 0) "N" * alleleLength else "N" // FIXME
+              val allele = Allele.create(alleleString) // FIXME
+              StrAllele(allele=allele, alleleLength=alleleLength, count=count.toInt, unitLength=str.unitLength)
             case _ => throw new IllegalArgumentException(s"Could not parse ALLREADS entry: $keyVal")
           }
        }
 
-      collect(str, refCalls, calls).iterator.foreach { case (repeatLength, count) =>
-         counter.count(repeatLength, count)
+      val counterForGenotype = collect(refCalls, calls)
+
+      counterForGenotype.stutterCounter.iterator.foreach { case (repeatLength: Int, count: Long) =>
+        counter.stutterCounter.count(repeatLength, count)
+      }
+      counterForGenotype.alleleCounter.iterator.foreach { case (repeatLength, count) =>
+        counter.alleleCounter.count(repeatLength, count)
       }
     }
 
@@ -220,27 +232,45 @@ class CollectStutterMetrics
   }
 }
 
+private object StutterAndAlleleCounter {
+  def empty: StutterAndAlleleCounter = StutterAndAlleleCounter()
+}
+
+private case class StutterAndAlleleCounter(stutterCounter: NumericCounter[Int] = new NumericCounter[Int](),
+                                           alleleCounter: SimpleCounter[String] = new SimpleCounter[String]())
+
 private object StutterCounter {
   val AllStrsKey: String = "all_strs"
 }
 
 private class StutterCounter(stutters: Seq[Int] = Seq.range(-5,6)) {
-  private val counters = mutable.HashMap[String,NumericCounter[Int]]()
+  private val stutterCounters = mutable.HashMap[String,NumericCounter[Int]]()
+  private val alleleCounters  = mutable.HashMap[String,SimpleCounter[String]]()
 
-  def add(name: String, counter: NumericCounter[Int]): Unit = {
-    if (counters.contains(name)) {
-      val strCounter = counters(name)
-      counter.iterator.foreach { case (stutter, count) => strCounter.count(stutter, count) }
+  def add(name: String, counters: StutterAndAlleleCounter): Unit = {
+    if (stutterCounters.contains(name) ) {
+      val counter = stutterCounters(name)
+      counters.stutterCounter.iterator.foreach { case (stutter, count) =>
+        counter.count(stutter, count)
+      }
     }
     else {
-      counters(name) = counter
+      stutterCounters(name) = counters.stutterCounter
+    }
+
+    if (alleleCounters.contains(name)) {
+      val counter = alleleCounters(name)
+      counters.alleleCounter.iterator.foreach { case (allele, count) => counter.count(allele, count) }
+    }
+    else {
+      alleleCounters(name) = counters.alleleCounter
     }
   }
 
-  def write(path: FilePath, strIntervals: Seq[StrInterval], minZeroStutter: Double): Unit = {
+  def write(prefix: PathPrefix, strIntervals: Seq[StrInterval], minZeroStutter: Double): Unit = {
     // Aggregate the counts across all STRs
     val allStrCounter = new NumericCounter[Int]()
-    counters.foreach { case (_, counter) =>
+    stutterCounters.foreach { case (_, counter) =>
       // Do not include sites where the fraction of reads/calls for zero stutters is too low.  This may be caused by
       // having the wrong genotype.
       val totalCounts = counter.map(_._2).sum
@@ -249,16 +279,63 @@ private class StutterCounter(stutters: Seq[Int] = Seq.range(-5,6)) {
         counter.foreach { case (stutter, count) => allStrCounter.count(stutter, count) }
       }
     }
-    counters(StutterCounter.AllStrsKey) = allStrCounter
+    stutterCounters(StutterCounter.AllStrsKey) = allStrCounter
 
-    // Print the metrics output
-    val names    = strIntervals.map(_.name) ++ Seq("all_strs")
-    val header   = (Seq("STR") ++ stutters).mkString("\t")
-    val lines = names.map { name =>
-      val counter = counters(name)
-      val values = stutters.map { stutter => counter.countOf(stutter) }
-      (Seq(name) ++ values).mkString("\t")
+    // Print the stutter metrics output
+    {
+      val names = strIntervals.map(_.name) ++ Seq("all_strs")
+      val regions = strIntervals.map { str => str.name -> s"${str.chrom}:${str.start}-${str.end}" }.toMap
+      val header = (Seq("STR", "REGION") ++ stutters).mkString("\t")
+      val lines = names.map { name =>
+        val region  = if (regions.contains(name)) regions(name) else ""
+        val counter = stutterCounters(name)
+        val values  = stutters.map { stutter => counter.countOf(stutter) }
+        (Seq(name, region) ++ values).mkString("\t")
+      }
+      val path = PathUtil.pathTo(prefix + ".stutter.tab")
+      Io.writeLines(path, Seq(header) ++ lines)
     }
-    Io.writeLines(path, Seq(header) ++ lines)
+
+    // Print the allele metrics output
+    {
+      val header = (Seq("STR", "REGION") ++ stutters).mkString("\t")
+      val lines = strIntervals.map { str =>
+        val name   = str.name
+        val region = s"${str.chrom}:${str.start}-${str.end}"
+        val counter = alleleCounters(name)
+        val values = counter.iterator.map { case (allele, count) => s"${prettyPrintAllele(str, allele)}\t$count" }
+        (Seq(name, region) ++ values).mkString("\t")
+      }
+      val path = PathUtil.pathTo(prefix + ".allele_counts.tab")
+      Io.writeLines(path, Seq(header) ++ lines)
+    }
+  }
+
+  private case class RepeatCounts(repeat: String, count: Int) {
+    override def toString: String = {
+      s"[${repeat.toUpperCase}]$count"
+    }
+  }
+
+  private def prettyPrintAllele(str: StrInterval, allele: String) = {
+    val counts = allele.grouped(str.unitLength).foldLeft(Seq[RepeatCounts]()) { case (repeatCounts: Seq[RepeatCounts], repeat: String) =>
+        repeatCounts.lastOption.filter(_.repeat == repeat).map { previous =>
+          repeatCounts.dropRight(1) :+ previous.copy(count=previous.count+1)
+        }.getOrElse {
+          repeatCounts :+ RepeatCounts(repeat, 1)
+        }
+      }
+    val countString = if (allele.forall(_ == 'N')) "Not Accessible" else counts.mkString("")
+
+    // NB: the repeat length is relative to the reference allele length in the VCF
+    val alleleLength = counts.map { rc => rc.repeat.length * rc.count }.sum
+    val repeatLength = if (alleleLength % str.unitLength == 0) {
+      f"${alleleLength / str.unitLength}%d"
+    }
+    else {
+      f"${alleleLength/ str.unitLength.toDouble}%.2f"
+    }
+
+    countString + "\t" + repeatLength
   }
 }
